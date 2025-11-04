0f10b34961f4fe896e7b9743e91332e6ed5e8885
Geekybot <uputpal16@gmail.com>
2025-11-04 13:51:13 +0530

    api route updated for /docs

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
