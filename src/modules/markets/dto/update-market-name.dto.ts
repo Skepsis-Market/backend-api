e2053901cb46b44479c2559f398612c7228073fe
Geekybot <uputpal16@gmail.com>
2025-12-16 09:48:03 +0530

    patch market name update

--- src/modules/markets/dto/update-market-name.dto.ts
+++ src/modules/markets/dto/update-market-name.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdateMarketNameDto {
   @ApiProperty({ 
     description: 'New market name',
     example: 'Bitcoin Price Prediction'
   })
+  @IsString()
+  @IsNotEmpty()
   marketName: string;
 }
